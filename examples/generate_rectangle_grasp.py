0053a848b40121115e9a344aab2f9043f37d3303
GouMinghao <gmh2015@163.com>
2020-10-06 12:47:14 +0800

    update generate rectangle labels

--- examples/generate_rectangle_grasp.py
+++ examples/generate_rectangle_grasp.py
@@ -12,9 +12,11 @@ from tqdm import tqdm
 
 
 
-def generate_scene_rectangle_grasp(sceneId, dump_folder, camera, graspnet = None, grasp_labels = None, collision_labels = None):
-    if graspnet is None:
-        graspnet = GraspNet(graspnet_root, camera=camera, split='all')
+def generate_scene_rectangle_grasp(sceneId, dump_folder, camera):
+    g = GraspNet(graspnet_root, camera=camera, split='all')
+    objIds = g.getObjIds(sceneIds = sceneId)
+    grasp_labels = g.loadGraspLabels(objIds)
+    collision_labels = g.loadCollisionLabels(sceneIds = sceneId)
     scene_dir = os.path.join(dump_folder,'scene_%04d' % sceneId)
     if not os.path.exists(scene_dir):
         os.mkdir(scene_dir)
@@ -22,25 +24,26 @@ def generate_scene_rectangle_grasp(sceneId, dump_folder, camera, graspnet = None
     if not os.path.exists(camera_dir):
         os.mkdir(camera_dir)
     for annId in tqdm(range(256), 'Scene:{}, Camera:{}'.format(sceneId, camera)):
-        _6d_grasp = graspnet.loadGrasp(sceneId = sceneId, annId = annId, format = '6d', camera = camera, grasp_labels = grasp_labels, collision_labels = collision_labels, fric_coef_thresh = 1.0)
+        _6d_grasp = g.loadGrasp(sceneId = sceneId, annId = annId, format = '6d', camera = camera, grasp_labels = grasp_labels, collision_labels = collision_labels, fric_coef_thresh = 1.0)
         rect_grasp_group = _6d_grasp.to_rect_grasp_group(camera)
         rect_grasp_group.save_npy(os.path.join(camera_dir, '%04d.npy' % annId))
 
 if __name__ == '__main__':
     ####################################################################
-    graspnet_root = '/home/gmh/graspnet'  ### ROOT PATH FOR GRASPNET ###
+    graspnet_root = '/home/minghao/graspnet'  ### ROOT PATH FOR GRASPNET ###
     ####################################################################
+
+    from multiprocessing import Pool
 
     dump_folder = 'rect_labels'
     if not os.path.exists(dump_folder):
         os.mkdir(dump_folder)
+    pool = Pool(24)
 
-    for camera in ['kinect', 'kinect']:
+    for camera in ['realsense', 'kinect']:
         # initialize a GraspNet instance
-        for sceneId in range(TOTAL_SCENE_NUM):
-            g = GraspNet(graspnet_root, camera=camera, split='all')
-            objIds = g.getObjIds(sceneIds = sceneId)
-            grasp_labels = g.loadGraspLabels(objIds)
-            collision_labels = g.loadCollisionLabels(sceneIds = sceneId)
-            generate_scene_rectangle_grasp(sceneId = sceneId, dump_folder = dump_folder, camera = camera, graspnet = g, grasp_labels = grasp_labels, collision_labels = collision_labels)
-
+        for sceneId in range(120):
+            pool.apply_async(func = generate_scene_rectangle_grasp, args = (sceneId, dump_folder, camera))
+    pool.close()
+    pool.join()
+    
