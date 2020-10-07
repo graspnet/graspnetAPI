b3fc1d326faeaab17a1e6da4ff62814c6d1e4f3d
gmh <GouMinghao>
2020-10-07 16:47:17 +0800

    update rect patch

--- copy_rect_labels.py
+++ copy_rect_labels.py
@@ -7,8 +7,8 @@ rect_labels_root = 'rect_labels'
 for sceneId in tqdm(range(190), 'Copying Labels'):
     for camera in ['kinect', 'realsense']:
         dest_dir = os.path.join(graspnet_root, 'scenes', 'scene_%04d' % sceneId, camera, 'rect')
-        rm_dest_dir = os.path.join(graspnet_root, 'scenes', 'scene_%04d' % sceneId, camera, 'rectangle_grasp')
-        os.system('rm -rf {}'.format(rm_dest_dir))
+        # rm_dest_dir = os.path.join(graspnet_root, 'scenes', 'scene_%04d' % sceneId, camera, 'rectangle_grasp')
+        # os.system('rm -rf {}'.format(rm_dest_dir))
         src_dir = os.path.join(rect_labels_root, 'scene_%04d' % sceneId, camera)
         if not os.path.exists(dest_dir):
             os.mkdir(dest_dir)
