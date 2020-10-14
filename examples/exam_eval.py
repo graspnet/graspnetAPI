fd0be1f8fee88079f5e3d35a4abb94198b1a7a69
GouMinghao <gmh2015@163.com>
2020-10-14 13:15:40 +0800

    modify eval

--- examples/exam_eval.py
+++ examples/exam_eval.py
@@ -8,15 +8,20 @@ from graspnetAPI import GraspNetEval
 
 ####################################################################
 # graspnet_root = '/home/gmh/graspnet' # ROOT PATH FOR GRASPNET
-graspnet_root = '/DATA1/Benchmark/graspnet' # ROOT PATH FOR GRASPNET
+graspnet_root = '/home/minghao/graspnet' # ROOT PATH FOR GRASPNET
 # dump_folder = '/home/minghao/hdd/dump_new'
-dump_folder = '/home/minghao/hdd/dump_new_pretrained'
+dump_folder = '/home/minghao/ssd/multi_dump/new_grasp-0'
 ####################################################################
 
 if __name__ == '__main__':
     sceneId = 160
     camera = 'kinect'    
     ge = GraspNetEval(root = graspnet_root, camera = camera, split = 'test')
+    
+    print('Evaluating scene:{}, camera:{}'.format(sceneId, camera))
     acc = ge.eval_scene(scene_id = sceneId, camera = camera, dump_folder = dump_folder)
     np_acc = np.array(acc)
     print('mean accuracy:{}'.format(np.mean(np_acc)))
+
+    print('Evaluating All')
+    ge.eval_all(dump_folder, proc = 24)
